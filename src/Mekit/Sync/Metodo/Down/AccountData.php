341b8aa0a477c5ba614dd4ae74ccfc6b473d86cf
jakabadambalazs <jakabadambalazs@gmail.com>
2015-12-02 15:46:27 +0100

    importing...accounts - needs duplicate control

--- src/Mekit/Sync/Metodo/Down/AccountData.php
+++ src/Mekit/Sync/Metodo/Down/AccountData.php
@@ -40,22 +40,27 @@ class AccountData {
 
     protected function elaborateIndexItems() {
         $i = 0;
-        $maxRun = 1;
-        foreach($this->index as $indexItem) {
-            if($i++ >= $maxRun) {
+        $maxRun = 999999999;
+        //foreach($this->index as $indexItem) {
+        while($indexItem = array_pop($this->index)) {
+            if($i >= $maxRun) {
                 $this->log("Reached hard limit($maxRun).");
                 break;
             }
-            $codice_metodo = $indexItem["codice_metodo"];
-            $this->log("elab item with codice_metodo: '" . $codice_metodo . "'");
-            $localItem = $this->loadLocalItem($codice_metodo);
-            //print_r($localItem);
-            $remoteItem = $this->loadRemoteItem($codice_metodo);
-            //print_r($remoteItem);
+//            if($indexItem["Nome1"] != "MEKIT SCS") {
+//                continue;
+//            }
+
+            $this->log("------------------------------------------------------------($i):" . $indexItem["CodiceMetodo"]);
+            $localItem = $this->loadLocalItem($indexItem);
+            //$this->log("Local item: " . json_encode($localItem));
+            $remoteItem = $this->loadRemoteItem($indexItem);
+            //$this->log("Remote item: " . json_encode($remoteItem));
             $syncItem = $this->createSyncItem($localItem, $remoteItem);
-            //print_r($syncItem);
-            //$res = $this->saveRemoteItem($syncItem);
-            //print_r($res);
+            //$this->log("Sync item: " . json_encode($syncItem));
+            $savedItem = $this->saveRemoteItem($syncItem);
+            //$this->log("Saved item: " . json_encode($savedItem));
+            $i++;
         }
     }
 
@@ -83,68 +88,128 @@ class AccountData {
      * @param bool|\stdClass    $localItem
      * @param bool|\stdClass    $remoteItem
      * @return bool|Array
+     * @throws \Exception
      */
     protected function createSyncItem($localItem, $remoteItem) {
+
         $syncItem = false;
         if($localItem) {
             $syncItem = [];
             if($remoteItem) {
-                $syncItem["id"] = $remoteItem->id;
-                //@todo: map other stuff?
+                //@todo: confront local(DataDiModifica) and remote(metodo_last_update_time_c) dates
+                //Skip ONLY if CodiceMetodo iIS present on remoteItem
+                if(true) {
+                    //local items has more recent date
+                    $syncItem["id"] = $remoteItem->id;
+                    //@todo: map other stuff?
+
+                }
+
+
             }
-            $syncItem["name"] = $localItem->RagioneSociale;
-            $syncItem["sync_codes_c"] = $localItem->CodiceMetodo;
+            //CODICE METODO
+            switch($localItem->database) {
+                case "IMP":
+                    switch($localItem->Tipologia) {
+                        case "C":
+                            $syncItem["metodo_client_code_imp_c"] = $localItem->CodiceMetodo;
+                            break;
+                        case "F":
+                            $syncItem["metodo_supplier_code_imp_c"] = $localItem->CodiceMetodo;
+                            break;
+                        default:
+                            throw new \Exception("Local item needs to have Tipologia C|F!");
+                    }
+                    break;
+                case "MEKIT":
+                    switch($localItem->Tipologia) {
+                        case "C":
+                            $syncItem["metodo_client_code_mekit_c"] = $localItem->CodiceMetodo;
+                            break;
+                        case "F":
+                            $syncItem["metodo_supplier_code_mekit_c"] = $localItem->CodiceMetodo;
+                            break;
+                        default:
+                            throw new \Exception("Local item needs to have Tipologia C|F!");
+                    }
+                    break;
+                default:
+                    throw new \Exception("Local item needs to have database IMP|MEKIT!");
+            }
+
+            $syncItem["name"] = $localItem->Nome1 . (!empty($localItem->Nome2) ? ' - ' . $localItem->Nome2 : '');
             $syncItem["codice_fiscale_c"] = $localItem->CodiceFiscale;
-            $syncItem["partita_iva_c"] = $localItem->PartitaIVA;
+            $syncItem["partita_iva_c"] = $localItem->PartitaIva;
+
+
+//            $syncItem["sync_codes_c"] = $localItem->CodiceMetodo;
+//            $syncItem["codice_fiscale_c"] = $localItem->CodiceFiscale;
+//            $syncItem["partita_iva_c"] = $localItem->PartitaIVA;
         }
         return $syncItem;
     }
 
     /**
-     * @param string $codice_metodo
+     * @param array $item
      * @return \stdClass|bool
      */
-    protected function loadRemoteItem($codice_metodo) {
+    protected function loadRemoteItem($item) {
         $itemData = false;
         $arguments = [
             "filter" => [
                 [
-                    "sync_codes_c" => $codice_metodo,
-                ],
+                    '$or' => [
+                                 ["partita_iva_c" => $item["PartitaIva"]],
+                                 ["codice_fiscale_c" => $item["CodiceFiscale"]],
+                                 ["metodo_client_code_imp_c" => $item["CodiceMetodo"]],
+                                 ["metodo_supplier_code_imp_c" => $item["CodiceMetodo"]],
+                                 ["metodo_client_code_mekit_c" => $item["CodiceMetodo"]],
+                                 ["metodo_supplier_code_mekit_c" => $item["CodiceMetodo"]],
+                             ],
+                ]
             ],
             "max_num" => 1,
             "offset" => 0,
-            "fields" => "id",
+            "fields" => "id,metodo_last_update_time_c",
         ];
         $result = $this->sugarCrmRest->comunicate('/Accounts/filter', 'GET', $arguments);
         if(isset($result->records) && count($result->records)) {
             $itemData = $result->records[0];
-            //$itemData = $this->sugarCrmRest->comunicate('/Accounts/' . $id, 'GET');
         }
-
         return($itemData);
     }
 
     /**
-     * @param string $codice_metodo
+     * For now $item is sufficient for local item
+     *
+     * @param array $item
      * @return \stdClass
      */
-    protected function loadLocalItem($codice_metodo) {
+    protected function loadLocalItem($item) {
+        /*
         $db = Configuration::getDatabaseConnection("SERVER2K8");
-        $query = "SELECT * FROM [IMP].[dbo].[SogCRM_AnagraficaCF] WHERE [CodiceMetodo] = '" . $codice_metodo . "'";
+        $query = "SELECT * FROM [IMP].[dbo].[SogCRM_AnagraficaCF] WHERE [CodiceMetodo] = '" . $item["CodiceMetodo"] . "'";
         $statement = $db->prepare($query);
         $statement->execute();
         $itemData = $statement->fetch(\PDO::FETCH_OBJ);
+        */
+        $itemData = (object)$item;
         return($itemData);
     }
 
-    protected function loadIndex() {
+    /**
+     * @param string $database IMP|MEKIT
+     */
+    protected function loadIndex($database="IMP") {
         $db = Configuration::getDatabaseConnection("SERVER2K8");
-        $query = "SELECT [CodiceMetodo] AS codice_metodo FROM [IMP].[dbo].[SogCRM_AnagraficaCF]";
+        $query = "SELECT * FROM [$database].[dbo].[ABJ_CLI_FOR_INDEX]";
         $statement = $db->prepare($query);
         $statement->execute();
-        $this->index = $statement->fetchAll(\PDO::FETCH_ASSOC);
-        $this->log("number of elements: " . count($this->index));
+        $index = $statement->fetchAll(\PDO::FETCH_ASSOC);
+        foreach($index as &$item) {
+            $item["database"] = $database;
+        }
+        $this->index = $index;
     }
 
     protected function log($msg) {
