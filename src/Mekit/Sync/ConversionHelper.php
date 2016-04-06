ebc65549bed0270338971de930834fe793b2587b
jakabadambalazs <jakabadambalazs@gmail.com>
2016-04-06 16:10:39 +0200

    comments

--- src/Mekit/Sync/ConversionHelper.php
+++ src/Mekit/Sync/ConversionHelper.php
@@ -18,7 +18,9 @@ class ConversionHelper {
      * @return mixed
      */
     public static function cleanupMSSQLFieldValue($value) {
+        $value = self::codepoint_decode($value);
         $value = htmlspecialchars_decode($value, ENT_QUOTES);//convert "&#039;" back to "'"
+        $value = htmlspecialchars_decode($value, ENT_NOQUOTES);//convert "&quot;" back to '"'
         return $value;
     }
 
@@ -34,4 +36,21 @@ class ConversionHelper {
         }
         return $value;
     }
+
+    /**
+     * @param string $str
+     * @return string
+     */
+    public static function codepoint_encode($str) {
+        return substr(json_encode($str), 1, -1);
+    }
+
+    /**
+     * @param string $str
+     * @return mixed
+     */
+    public static function codepoint_decode($str) {
+        return json_decode(sprintf('"%s"', $str));
+    }
+
 }
